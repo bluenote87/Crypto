f691502ba58ea72dada700e129b261790ce1e0e4
Paul Fannon <lurchworld@gmail.com>
2018-02-26 14:36:30 -0600

    Helper functions
    Moved two reusable functions into a helper file and implemented into ciphers via import.

--- caesar.py
+++ caesar.py
@@ -1,34 +1,4 @@
-def alphabet_position(letter):
-    result = 0
-    idx = 0
-    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
-                "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
-                "u", "v", "w", "x", "y", "z"]
-    for char in alphabet:
-        if letter.lower() == char:
-            result = idx
-        idx = idx + 1
-        
-    return result
-
-def rotate_character(char, rot):
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    ALPHA_bet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    result = char
-    if char in alphabet:
-        rotation = alphabet_position(char) + rot
-        if rotation < 26:
-            result = alphabet[rotation]
-        else:
-            result = alphabet[rotation % 26]
-    if char in ALPHA_bet:
-        rotation = alphabet_position(char) + rot
-        if rotation < 26:
-            result = ALPHA_bet[rotation]
-        else:
-            result = ALPHA_bet[rotation % 26]
-    
-    return result
+from helpers import alphabet_position, rotate_character
 
 def encrypt(text, rot):
     result = ""
